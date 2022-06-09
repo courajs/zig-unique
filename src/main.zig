c5d4189ddd226e0ead4811204dcbaac48159c533
Aaron Sikes <aaron@sikes.io>
2022-06-08 22:43:17 -0400

    Some experiments!
    I can use @Type(@typeInfo()), but only for a struct with no decls
    
    I can use `packed` and `@bitCast`
    
    I can use `@ptrCast`, and if the structure is the same it
    seems to work. Seems the option most likely to break.
    
    See also: https://github.com/ziglang/zig/issues/1595

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,46 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+fn Different(comptime T: type, comptime uniq: type) type {
+    _ = uniq;
+    return @Type(@typeInfo(T));
 }
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+const A = struct {
+    a: u32,
+
+    // pub fn inc(self: @This()) @This() {
+    //     return @This(){
+    //         .a = self.a + 1,
+    //     };
+    // }
+};
+const Thing = struct {};
+const B = Different(A, Thing);
+const C = Different(A, struct {});
+
+const Q = struct {
+    a: u32,
+
+    pub fn inc(self: @This()) @This() {
+        return @This(){
+            .a = self.a + 1,
+        };
+    }
+};
+const R = struct {
+    a: u32,
+
+    pub fn onc(self: @This()) @This() {
+        return @This(){
+            .a = self.a + 1,
+        };
+    }
+};
+
+pub fn main() anyerror!void {
+    // std.log.info("Equal? {}", .{B == C});
+    // std.log.info("A,B,C: {}, {}, {}", .{ A, B, C });
+    var b = B{ .a = 42 };
+    var a = @ptrCast(*A, &b);
+    std.log.info("a val {}", .{a});
 }
